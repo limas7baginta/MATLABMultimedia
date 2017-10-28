6c207a879a44cbbb58811a544e553b9ec88f1219
Limas Baginta <limas.baginta@ui.ac.id>
2017-10-29 03:36:53 +0700

    Add files via upload

---
+++ LimasBaginta_1306368690_Workshop2.m
@@ -0,0 +1,34 @@
+%Workshop2_Limas Baginta_1306368690
+
+gambar1 = imfinfo ('imagefile1')
+
+gambar2 = imfinfo ('imagefile2')
+
+gambar3 = imfinfo ('imagefile3')
+
+gambar4 = imfinfo ('imagefile4')
+
+gambar5 = imfinfo ('imagefile5')
+
+gambar6 = imfinfo ('imagefile6')
+
+gambar7 = imfinfo ('imagefile7')
+
+gambar8 = imfinfo ('imagefile8')
+
+
+subplot(331);imshow ('imagefile1'); 
+
+subplot(332);imshow ('imagefile2'); 
+
+subplot(333);imshow ('imagefile3'); 
+
+subplot(334);imshow ('imagefile4'); 
+
+subplot(335);imshow ('imagefile5'); 
+
+subplot(336);imshow ('imagefile6'); 
+
+subplot(337);imshow ('imagefile7'); 
+
+subplot(338);imshow ('imagefile8'); 
